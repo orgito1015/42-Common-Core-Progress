0ad256bfae7aa369a1e93d545190644287a7211f
PR0F3550R <86354243+orgito1015@users.noreply.github.com>
2025-12-02 00:56:57 +0100

    Clean up comments in ft_swap function
    Removed comments explaining the swap process.

--- ExamProgrammingFundamentals/ft_swap.c
+++ ExamProgrammingFundamentals/ft_swap.c
@@ -1,13 +1,8 @@
 void ft_swap(int *a, int *b)
 {
-    /* we have to declare a temporary variable
-     * to make a swap between 2 variables
-     */
+
     int x;
-    // set our temporary variable to be equal to the value of *a
     x = *a;
-    // set the value of *a to be equal to the value of *b
     *a = *b;
-    // set the value of *b to be equal to the value of x
     *b = x;
 }
