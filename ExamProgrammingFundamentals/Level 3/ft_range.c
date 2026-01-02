62041f5e54535469d35b563fc5f0a86f5b31b81e
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-02 19:48:31 +0100

    Update ft_range.c

--- ExamProgrammingFundamentals/Level 3/ft_range.c
+++ ExamProgrammingFundamentals/Level 3/ft_range.c
@@ -1,44 +1,26 @@
 #include <stdlib.h>
 
-static int	my_ft_strlen(char *str)
+int *ft_range(int start, int end)
 {
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strdup(char *str)
-{
-	char	*ret;
-	int		len;
-
-	len = my_ft_strlen(str);
-	ret = malloc(sizeof(*ret) * (len + 1));
-	if (ret == 0)
-		return (ret);
-	ret[len] = 0;
-	while (len > 0)
-	{
-		ret[len - 1] = str[len - 1];
-		len = len - 1;
-	}
-	return (ret);
-}
-
-int		*ft_range(int start, int end)
-{
-	int	step = (start > end ? -1 : 1);
-	int	size = (start > end ? start - end + 1 : end - start + 1);
-	int	*ret = malloc(sizeof(*ret) * size);
-	int	i;
-
-	for (i = 0; i < size; i++)
-	{
-		ret[i] = start;
-		start += step;
-	}
-	return (ret);
+    int i = 0;
+    // Defining the lenght of the range
+    // Since we don't have access to the abs function, we have
+    // to make a manual absolute value
+    int len = (end - start) < 0 ? ((end - start) * -1) + 1 : (end - start) + 1;
+    // Allocating the range needed
+    int *range = (int *) malloc(len * sizeof(int));
+    
+    // Fill in the range
+    while (i < len)
+    {
+        // Next lines are for numbers going up
+        if (start < end)
+            range[i] = start++;
+        // Next lines are for numbers going down
+        else
+            range[i] = start--;
+        i++;
+    }
+    // Returning the filled range
+    return (range);
 }
