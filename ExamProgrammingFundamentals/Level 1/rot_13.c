283af6cdd71605d0db0e302ca3fcc44c49de8b2e
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-04 14:47:44 +0100

    Improve ROT13 encoding logic
    Refactor ROT13 implementation for clarity and efficiency.

--- ExamProgrammingFundamentals/Level 1/rot_13.c
+++ ExamProgrammingFundamentals/Level 1/rot_13.c
@@ -1,37 +1,24 @@
-int main(int argc, char **argv)
-{
-    int i;    if (argc == 2)
-    {
-        i = 0;
-        while (argv[1][i])
-        {
-            if ((argv[1][i] >= 'a' && argv[1][i] <= 'm') || (argv[1][i] >= 'A' && argv[1][i] <= 'M'))
-                argv[1][i] += 13;
-            else if ((argv[1][i] >= 'n' && argv[1][i] <= 'z') || (argv[1][i] >= 'N' && argv[1][i] <= 'Z'))
-                argv[1][i] -= 13;
-            write(1, &argv[1][i], 1);
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return (0);
-}
+#include <unistd.h>
 
-int main(int argc, char **argv)
+int			main(int argc, char **argv)
 {
-    int i;    if (argc == 2)
-    {
-        i = 0;
-        while (argv[1][i])
-        {
-            if ((argv[1][i] >= 'a' && argv[1][i] <= 'm') || (argv[1][i] >= 'A' && argv[1][i] <= 'M'))
-                argv[1][i] += 13;
-            else if ((argv[1][i] >= 'n' && argv[1][i] <= 'z') || (argv[1][i] >= 'N' && argv[1][i] <= 'Z'))
-                argv[1][i] -= 13;
-            write(1, &argv[1][i], 1);
-            i++;
-        }
-    }
-    write(1, "\n", 1);
-    return (0);
+	int		i;
+	char	c;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i])
+		{
+			c = argv[1][i];
+			if (c <= 'Z' && c >= 'A')
+				c = (c - 'A' + 13) % 26 + 'A';
+			else if (c <= 'z' && c >= 'a')
+				c = (c - 'a' + 13) % 26 + 'a';
+			write(1, &c, 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
