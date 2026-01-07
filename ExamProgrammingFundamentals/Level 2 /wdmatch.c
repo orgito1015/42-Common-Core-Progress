525ba671b246d1d4792107c94f9f15aa4f0ebeb7
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-07 16:02:34 +0100

    Update wdmatch.c

--- ExamProgrammingFundamentals/Level 2 /wdmatch.c
+++ ExamProgrammingFundamentals/Level 2 /wdmatch.c
@@ -1,28 +1,19 @@
 #include <unistd.h>
 
-static char		*wdmatch(char *str1, const char *str2)
+int main(int argc, char **argv)
 {
-	while (*str1 != '\0' && *str2 != '\0')
-	{
-		if (*str1 == *str2)
-			str1++;
-		str2++;
-	}
-	if (*str1 == '\0')
-		return (str1);
-	return (NULL);
-}
-
-int				main(int argc, char **argv)
-{
-	char			*tmp;
-
-	if (argc == 3)
-	{
-		tmp = wdmatch(argv[1], argv[2]);
-		if (tmp != NULL)
-			write(1, argv[1], tmp - argv[1]);
-	}
-	write(1, "\n", 1);
-	return (0);
+    int i = 0;
+    int j = 0;
+    if (argc == 3)
+    {
+        while (argv[1][i] && argv[2][j])
+        {
+            if (argv[1][i] == argv[2][j])
+                i++;
+            j++;
+        }
+        if (argv[1][i] == '\0')
+            write(1, argv[1], i);
+    }
+    write(1, "\n", 1);
 }
