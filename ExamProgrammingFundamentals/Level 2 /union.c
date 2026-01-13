5b9d12bb090f5959a72206b3f4636c76b8714860
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-13 09:41:21 +0100

    Update union.c

--- ExamProgrammingFundamentals/Level 2 /union.c
+++ ExamProgrammingFundamentals/Level 2 /union.c
@@ -1,61 +1,31 @@
 #include <unistd.h>
 
-int	ft_strlen(char *s1)
+int main(int ac, char *av[])
 {
-	int len;
-
-	len = 0;
-	while (s1[len])
-		len++;
-	return (len);
-}
-
-static void	remove_from(char *s1, int len, char c, int start)
-{
-	int	i;
-
-	i = start;
-	while (i < len)
+	if (ac == 3)
 	{
-		if (s1[i] == c)
-			s1[i] = 0;
-		i++;
-	}
-}
+		int i = 0;
+		int j = 0;
+		int seen[256] = {};
 
-static void check_1(char *s1, char *s2, int len1, int len2)
-{
-	int i;
-
-	i = 0;
-	while (i < len1)
-	{
-		if (s1[i] != 0)
+		while(av[1][i])
 		{
-			remove_from(s1, len1, s1[i], i + 1);
-			remove_from(s2, len2, s1[i], 0);
-			write(1, s1 + i, 1);
+			if(!seen[(int)av[1][i]])
+			{
+				seen[(int)av[1][i]] = 1;
+				write(1, &av[1][i], 1);
+			}
+			i++;
 		}
-		i = i + 1;
+		while (av[2][j])
+		{
+			if(!seen[(int)av[2][j]])
+			{
+				seen[(int)av[2][j]] = 1;
+				write(1, &av[2][j], 1);
+			}
+			j++;
+		}
 	}
-
-}
-
-void	ft_union(char *s1, char *s2)
-{
-	int	len1;
-	int	len2;
-
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	check_1(s1, s2, len1, len2);
-	check_1(s2, s1, len2, len1);
-}
-
-int	main(int argc, char **argv)
-{
-	if (argc == 3)
-		ft_union(argv[1], argv[2]);
 	write(1, "\n", 1);
-	return (0);
 }
