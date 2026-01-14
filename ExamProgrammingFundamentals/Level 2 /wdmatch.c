cf26a4b393ccf0d60023d896c5dbfd892ec46f8c
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-14 14:47:29 +0100

    Update wdmatch.c

--- ExamProgrammingFundamentals/Level 2 /wdmatch.c
+++ ExamProgrammingFundamentals/Level 2 /wdmatch.c
@@ -2,30 +2,24 @@
 
 int main(int ac, char **av)
 {
-    int i;
-	int j;
+    int i = 0;
+    int j = 0;
 
-    i=0;
-	j=0;
-
-	if(ac == 3)
+    if (ac == 3)
+    {
+        // check if all characters of av[1] appear in order in av[2]
+        while (av[1][i] && av[2][j])
         {
-                while( av[1][i] && av[1][j])
-                {
-                        if(av[1][i] == av[1][j])
-                        i++;
-                j++;
-                }
+            if (av[1][i] == av[2][j])
+                i++;
+            j++;
         }
-	if( av[1][i]   == '\0')
-                {
-                        write(1, av[1], i);
-                }
 
+        // if we reached the end of av[1], all chars were matched
+        if (av[1][i] == '\0')
+            write(1, av[1], i);
+    }
 
-     write(1, "\n", 1);
-     return 0;
+    write(1, "\n", 1);
+    return 0;
 }
-
-
-
