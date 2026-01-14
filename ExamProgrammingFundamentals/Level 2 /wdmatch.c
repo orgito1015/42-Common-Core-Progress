b473faf5b50cdf0947ef613cd976ff3db40fe671
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-14 14:45:20 +0100

    Update wdmatch.c

--- ExamProgrammingFundamentals/Level 2 /wdmatch.c
+++ ExamProgrammingFundamentals/Level 2 /wdmatch.c
@@ -1,19 +1,31 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    int i = 0;
-    int j = 0;
-    if (argc == 3)
-    {
-        while (argv[1][i] && argv[2][j])
+        int i;
+	int j;
+
+        i=0;
+	j=0;
+
+	if(ac == 3)
         {
-            if (argv[1][i] == argv[2][j])
-                i++;
-            j++;
+                while( av[1][i] && av[1][j])
+                {
+                        if(av[1][i] == av[1][j])
+                        i++;
+                j++;
+                }
         }
-        if (argv[1][i] == '\0')
-            write(1, argv[1], i);
-    }
-    write(1, "\n", 1);
+	if( av[1][i]   == '\0')
+                {
+                        write(1, av[1], i);
+                }
+
+
+        write(1, "\n", 1);
+        return 0;
 }
+
+
+
