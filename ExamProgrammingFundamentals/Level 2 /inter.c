74803cdcd5f2c90870c2e0367228cc9109cab07f
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-15 13:57:15 +0100

    Add inter.c to find common characters in two strings

---
+++ ExamProgrammingFundamentals/Level 2 /inter.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+int ft_putchar(char c)
+{
+        write(1, &c, 1);
+}
+
+int main(int ac, char **av)
+{
+        int i;
+	i =0;
+
+	int j;
+
+        int lookup[256] = {};
+
+        if(ac == 3)
+        {
+
+                while(av[1][i])
+                {
+                        j=0;
+                        while(av[2][j])
+                        {
+                                if(av[1][i] ==av[2][j] && !lookup[(int)av[2][j]])
+                                {
+                                lookup[(int)av[2][j]]=1;
+                                ft_putchar(av[2][j]);
+
+                                }
+                                j++;
+                        }
+                        i++;
+                }
+
+        }
+	ft_putchar('\n');
+}
+
+
+
+
+
