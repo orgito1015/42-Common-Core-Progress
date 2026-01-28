ddee3aef1d4a57d9c7e73beb7b41c99d298dfce9
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-28 14:08:35 +0100

    Update first_word.c

--- ExamProgrammingFundamentals/Level 1/first_word.c
+++ ExamProgrammingFundamentals/Level 1/first_word.c
@@ -1,24 +1,17 @@
-#include <unistd.h> // For the write() system call
+#include <unistd.h> 
 
 int main(int ac, char **av)
 {
-    int i; // Loop counter for iterating through the string
-    i = 0; // Start from the first character
+    int i; 
+    i = 0; 
 
-    // Check if exactly 1 argument is provided (program name + 1)
     if (ac == 2)
     {
-        // Skip all leading whitespace characters (space, tab, newline, etc.)
         while (av[1][i] && av[1][i] <= ' ')
             i++;
-
-        // Print characters until the next whitespace or end of string
         while (av[1][i] > ' ')
-            write(1, &av[1][i++], 1); // &av[1][i] gives the address of the character
+            write(1, &av[1][i++], 1); 
     }
-
-    // Print a newline at the end for proper formatting
     write(1, "\n", 1);
-
-    return 0; // Return 0 to indicate successful execution
+    return 0; 
 }
