c6ed0cc8bb61955156126687b0512a2c7b95356a
PR0F3550R1 <86354243+orgito1015@users.noreply.github.com>
2026-01-29 12:58:18 +0100

    Update inter.c

--- ExamProgrammingFundamentals/Level 2 /inter.c
+++ ExamProgrammingFundamentals/Level 2 /inter.c
@@ -13,25 +13,22 @@ int main(int ac, char **av)
 	
     int seen[256] = {};
 
-        if(ac == 3)
+    if(ac == 3)
+    {
+        while(av[1][i])
         {
-
-                while(av[1][i])
-                {
-                        j=0;
-                        while(av[2][j])
-                        {
-                                if(av[1][i] ==av[2][j] && !seen[(int)av[2][j]])
-                                {
-                                seen[(int)av[2][j]]=1;
-                                ft_putchar(av[2][j]);
-
-                                }
-                                j++;
-                        }
-                        i++;
-                }
-
+         j=0;
+         while(av[2][j])
+         {
+           if(av[1][i] ==av[2][j] && !seen[(int)av[2][j]])
+            {
+                seen[(int)av[2][j]]=1;
+                ft_putchar(av[2][j]);
+			}
+              j++;
+               }
+              i++;
+            }
         }
 	ft_putchar('\n');
 }
